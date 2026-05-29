b4da046596ddd657f3386e48f82575c06ee20b0e
Gurgen Baba <gurgenbaba.msi@outlook.de>
2026-05-29 23:49:01 +0200

    Update exchange daily limit to 500000000

--- game/exchange.py
+++ game/exchange.py
@@ -16,7 +16,7 @@ _EXCHANGE_SETTING_DEFAULTS = {
     "exchange_enabled": "1",
     "exchange_rate_metal_to_crystal": "0.8",
     "exchange_rate_crystal_to_metal": "0.8",
-    "exchange_daily_limit": "50000",
+    "exchange_daily_limit": "500000000",
     "exchange_min_amount": "100",
 }
 
